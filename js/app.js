d1025df3acde9a3051eff52b244067932b3cde3d
Heathcliff <dennis.nathan@gmail.com>
2016-10-11 16:17:29 -0600

    Uploaded finished assignment

--- js/app.js
+++ js/app.js
@@ -4,3 +4,15 @@
   $('select').material_select();
 
 })();
+
+var app = angular.module('cameraApp', []);
+
+app.filter('stars', function () {
+  return function (input) {
+    let returnString = '';
+    for (x = 0; x < input; x++) {
+      returnString += '*';
+    }
+    return returnString;
+  }
+});
